a518e1a21798b64c97d4aa9870d4baad83ad8ac8
Jon Dimaculangan <jondmclngn@gmail.com>
2022-04-08 18:27:29 -0500

    trial portfolio

---
+++ components/navbar.jsx
@@ -0,0 +1,17 @@
+// built in link for SSR optimization and SEO from next
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <Link href='/'>
+                {/* <a>Home</a> */}
+                Home
+            </Link>
+
+            <Link href='/about'>
+                About me
+            </Link>
+        </nav>
+    )
+}
