a31fa6ee1e18cdb8f55e37cff331ed4303e8b213
Jon Dimaculangan <jondmclngn@gmail.com>
2022-04-24 18:13:25 -0500

    mobile responsiveness added

--- components/about.jsx
+++ components/about.jsx
@@ -2,7 +2,6 @@
 import Image from 'next/image'
 import styles from '../styles/About.module.css'
 
-
 export default function About() {
     return (
         <div className='container' >
@@ -17,19 +16,13 @@ export default function About() {
                         height={400}
                     />
                     <h1 className={styles.tealText}>Jon Dimaculangan</h1>
-                    {/* <Image
-                        src="/vercel.svg"
-                        alt="the vercel logo bc I <3 vercel"
-                        width={500}
-                        height={500}
-                    /> */}
                 </section>
                 <section id='desc' className={`center column ${styles.container}`}>
                     <p>A career working with people in a fast-paced, quality-driven environment has honed me to have intensity and integrity in my work while maintaining high-quality results.</p>
                     <p>Curiosity has always enveloped my mind, it provides me a hint of foresight that gives me the ability to plan and adapt accordingly.</p>
                     <p>I am able to communicate effectively by assessing the current problem as well as solving it by breaking it down into pieces.</p>
 
-                    <button className={styles.resumeBtn}>
+                    <button className={styles.resumeBtn} onClick={() => window.open('../documents/Jon_Resume.pdf')}>
                         RESUME
                     </button>
                 </section>
